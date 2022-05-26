0350b8c8edbc7add371a99b314e5773f812da6af
Mathieu Stenzel <mathieu.stenzel@web.de>
2022-05-26 14:14:41 +0200

    pip install

--- PSICO-Buerger-Software/pip.install.py
+++ PSICO-Buerger-Software/pip.install.py
@@ -4,4 +4,5 @@ os.system("pip install psutil")
 os.system("pip install pandas")
 os.system("pip install win10toast")
 os.system("pip install opencv-python")
-
+os.system("pip install sounddevice")
+os.system("pip install scipy")
