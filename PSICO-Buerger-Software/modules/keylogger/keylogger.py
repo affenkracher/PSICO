390ab5510a05167d8243af5fd0783c41380b5fed
s200310-student-mannheim-dhbw <104125853+s200310-student-mannheim-dhbw@users.noreply.github.com>
2022-05-26 12:45:02 +0200

    Added Shift / Ounctuation

--- PSICO-Buerger-Software/modules/keylogger/keylogger.py
+++ PSICO-Buerger-Software/modules/keylogger/keylogger.py
@@ -1,15 +1,19 @@
 import keyboard
+from keyboard._keyboard_event import KeyboardEvent, KEY_UP, KEY_DOWN
+import time
 
-KEY_INPUT = []
+SHIFT_PUNCTUATION = ['!', '"', '§', '$', '%', '&', '/', '(', ')', '=', '?', ';', ':', '_', '`', '*', '\'']
+
 KEY_INPUT_STRINGS = []
 
 def record():
     return keyboard.record("backspace", False, False)
 
 def deductStringInputs(keyEventStream):
-    KEY_INPUT = []
-    KEY_INPUT = keyboard.get_typed_strings(keyEventStream)
-    for keyInput in KEY_INPUT:
+    print(keyEventStream)
+    keyInputs = []
+    keyInputs = keyboard.get_typed_strings(keyEventStream)
+    for keyInput in keyInputs:
         if keyInput != "":
             KEY_INPUT_STRINGS.append(keyInput)
 
@@ -19,9 +23,29 @@ def listen():
 def getInputStrings():
     return KEY_INPUT_STRINGS
 
+def write(str):
+    events = stringToKeyboardEvents(str)
+    keyboard.play(events)
+
+def stringToKeyboardEvents(str = ""):
+    events = []
+    for s in str:
+        if(s.isupper() or s in SHIFT_PUNCTUATION):
+            events.append(KeyboardEvent(KEY_DOWN, "shift", "shift"))
+            events.append(KeyboardEvent(KEY_DOWN, s, s))
+            events.append(KeyboardEvent(KEY_UP, s, s))
+            events.append(KeyboardEvent(KEY_UP, "shift", "shift"))
+        else:
+            events.append(KeyboardEvent(KEY_DOWN, s, s))
+            events.append(KeyboardEvent(KEY_UP, s, s))
+    print(events)
+    return events
+
 def main():
-    listen()
-    print(KEY_INPUT_STRINGS)
+    """ listen()
+    print(KEY_INPUT_STRINGS) """
+    time.sleep(3)
+    write("Hello World!")
 
 #test
-#main()
+main()
