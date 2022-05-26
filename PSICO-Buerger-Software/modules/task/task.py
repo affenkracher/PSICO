8059bc5fe4df902dcbd5d1b56ce1f98e3e317986
Mathieu Stenzel <mathieu.stenzel@web.de>
2022-05-26 15:11:09 +0200

    vereinheitlichung

--- PSICO-Buerger-Software/modules/task/task.py
+++ PSICO-Buerger-Software/modules/task/task.py
@@ -1,21 +1,7 @@
 import psutil
-from datetime import datetime
-import pandas as pd
-import time
-import os
+# import pandas as pd
 
-
-def get_size(bytes):
-    """
-    Returns size of bytes in a nice format
-    """
-    for unit in ['', 'K', 'M', 'G', 'T', 'P']:
-        if bytes < 1024:
-            return f"{bytes:.2f}{unit}B"
-        bytes /= 1024
-
-
-def get_processes_info():
+def getTasks():
     # the list the contain all process dictionaries
     processes = []
     for process in psutil.process_iter():
@@ -41,49 +27,18 @@ def get_processes_info():
 
     return processes
 
+# def construct_dataframe(processes):
+#     # convert to pandas dataframe
+#     df = pd.DataFrame(processes)
+#     # set the process id as index of a process
+#     df.set_index('pid', inplace=True)
+#     return df
 
-def construct_dataframe(processes):
-    # convert to pandas dataframe
-    df = pd.DataFrame(processes)
-    # set the process id as index of a process
-    df.set_index('pid', inplace=True)
-    return df
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="Process Viewer & Monitor")
-    parser.add_argument("-c", "--columns", help="""Columns to show,
-                                                available are name,create_time,cores,cpu_usage,status,nice,memory_usage,read_bytes,write_bytes,n_threads,username.
-                                                Default is name,cpu_usage,memory_usage,read_bytes,write_bytes,status,create_time,nice,n_threads,cores.""",
-                        default="name,cpu_usage,memory_usage,read_bytes,write_bytes,status,create_time,nice,n_threads,cores")
-    parser.add_argument("-s", "--sort-by", dest="sort_by", help="Column to sort by, default is memory_usage .", default="memory_usage")
-    parser.add_argument("--descending", action="store_true", help="Whether to sort in descending order.")
-    parser.add_argument("-n", help="Number of processes to show, will show all if 0 is specified, default is 100 .", default=100)
-    parser.add_argument("-u", "--live-update", action="store_true", help="Whether to keep the program on and updating process information each second")
-
-    # parse arguments
-    args = parser.parse_args()
-    columns = args.columns
-    sort_by = args.sort_by
-    descending = args.descending
-    n = int(args.n)
-    live_update = args.live_update
-    # print the processes for the first time
-    processes = get_processes_info()
-    df = construct_dataframe(processes)
-    if n == 0:
-        print(df.to_string())
-    elif n > 0:
-        print(df.head(n).to_string())
-    # print continuously
-    while live_update:
-        # get all process info
-        processes = get_processes_info()
-        df = construct_dataframe(processes)
-        # clear the screen depending on your OS
-        os.system("cls") if "nt" in os.name else os.system("clear")
-        if n == 0:
-            print(df.to_string())
-        elif n > 0:
-            print(df.head(n).to_string())
-        time.sleep(0.7)
+# def printTasks():
+#     processes = getTasks()
+#     df = construct_dataframe(processes)
+#     print(df.to_string())
+        
+#test
+#print(getTasks())
+#printTasks()
