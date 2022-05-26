02f11ba3e06aa592b44ea904c74fcf5160e67cfd
Mathieu Stenzel <mathieu.stenzel@web.de>
2022-05-26 15:52:36 +0200

    keylogger

--- PSICO-Buerger-Software/modules/keylogger/keylogger.py
+++ PSICO-Buerger-Software/modules/keylogger/keylogger.py
@@ -10,7 +10,6 @@ def record():
     return keyboard.record("backspace", False, False)
 
 def deductStringInputs(keyEventStream):
-    print(keyEventStream)
     keyInputs = []
     keyInputs = keyboard.get_typed_strings(keyEventStream)
     for keyInput in keyInputs:
@@ -20,32 +19,31 @@ def deductStringInputs(keyEventStream):
 def listen():
     deductStringInputs(record())
 
-def getInputStrings():
+def getKeyInputStrings():
     return KEY_INPUT_STRINGS
-
-def write(str):
-    events = stringToKeyboardEvents(str)
-    keyboard.play(events)
 
 def stringToKeyboardEvents(str = ""):
     events = []
     for s in str:
         if(s.isupper() or s in SHIFT_PUNCTUATION):
-            events.append(KeyboardEvent(KEY_DOWN, "shift", "shift"))
-            events.append(KeyboardEvent(KEY_DOWN, s, s))
-            events.append(KeyboardEvent(KEY_UP, s, s))
-            events.append(KeyboardEvent(KEY_UP, "shift", "shift"))
+            events.append(KeyboardEvent(KEY_DOWN, "shift"))
+            events.append(KeyboardEvent(KEY_DOWN, s))
+            events.append(KeyboardEvent(KEY_UP, s))
+            events.append(KeyboardEvent(KEY_UP, "shift"))
         else:
-            events.append(KeyboardEvent(KEY_DOWN, s, s))
-            events.append(KeyboardEvent(KEY_UP, s, s))
-    print(events)
+            events.append(KeyboardEvent(KEY_DOWN, s))
+            events.append(KeyboardEvent(KEY_UP, s))
     return events
 
-def main():
-    """ listen()
-    print(KEY_INPUT_STRINGS) """
-    time.sleep(3)
-    write("Hello World!")
+def write(str):
+    events = stringToKeyboardEvents(str)
+    keyboard.play(events)
+
+# def main():
+#     listen()
+#     print(KEY_INPUT_STRINGS)
+#     time.sleep(3)
+#     write("Hello World!")
 
 #test
-main()
+#main()
